6242f80438a5bf31d3b46c3507e1606bb768efe8
krishnaher0 <krishna613460@gmail.com>
2024-06-21 19:07:33 +0545

    Error solving while implementing crud functionalities

--- src/main/java/com/example/taskwebdev/Entity/User.java
+++ src/main/java/com/example/taskwebdev/Entity/User.java
@@ -13,7 +13,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.SEQUENCE,generator="users_seq_gen")
     @SequenceGenerator(name="users_seq_gen",sequenceName="users_seq",allocationSize=1)
     @Id
-    private Integer Id;
+    private Integer id;
     @Column(name="user_name" ,nullable=false, length=100)
     private String  userName;
 
